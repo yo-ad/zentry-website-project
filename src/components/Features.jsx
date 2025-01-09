ae4955365576e3ee13d575d8915bce5fa15204fb
yosef_adrian <yosef.adrian@unpar.ac.id>
2025-01-09 14:39:17 +0700

    continuing

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { TiLocationArrow } from 'react-icons/ti';
 
-// https://youtu.be/zA9r5zTllx4?t=6683
+// https://www.youtube.com/watch?v=zA9r5zTllx4&t=6683s
 
 const BentoCard = ({ src, title, description, isComingSoon }) => {
   return (
@@ -59,11 +60,51 @@ const Features = () => {
         <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7'>
           <div className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
             <BentoCard
-              src="videos/feature-1.mp4"
-              title={<>radia<b>n</b>t</>}
-              description="A cross-platform metagame app, turning your
-          activities across Web2 and Web3 games into a rewarding adventure."
+              src="videos/feature-2.mp4"
+              title={<>zigma</>}
+              description="An anime and gaming-inspired collection - 
+              the IP rimed for expansion."
               isComingSoon
+            />
+          </div>
+          <div className='bento_tilt_1 row-span-1 ms-32
+          md:col-span-1 md:ms-0'>
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title={<>n<b>e</b>xus</>}
+              description="A gamified social hub, adding a new dimension of play to social
+              interaction for Web3 communities."
+              isComingSoon
+            />
+          </div>
+
+          <div className='bento_tilt_1 me-14 md:col-span-1 md:me-0'>
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={<>az<b>u</b>re</>}
+              description="A cro0ss-world AI Agent - elevating your
+              gameplay to be more fun and productive."
+              isComingSoon
+            />
+          </div>
+
+          <div className='bento-tilt_2'>
+            <div className='flex size-full flex-col justify-between 
+          bg-violet-300 p-5'>
+              <h1 className='bento-title special-font max-w-64
+              text-black'>M<b>o</b>re co<b>m</b>ing s<b>o</b>on!</h1>
+
+              <TiLocationArrow className='m-5 scale-[5] self-end' />
+            </div>
+          </div>
+
+          <div className='bento-tilt_2'>
+            <video
+              src='/videos/feature-5.mp4'
+              loop
+              muted
+              autoPlay
+              className='size-full object-cover object-center'
             />
           </div>
         </div>
